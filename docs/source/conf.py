6fda6a341b2a831fa0ceac1511cd1eb5d125f635
Qubut <s-aahmed@haw-landshut.de>
2023-04-21 21:23:44 +0200

    feat: added Sphinx and Github actions
    added sphinx and configured in the project
    added related github actions to test poetry on differnt platforms
    and generate doc pages from the documentation
    added .gitignore file

---
+++ docs/source/conf.py
@@ -0,0 +1,49 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+import sphinx_rtd_theme
+
+# Path to the directory containing your Python modules
+sys.path.insert(0, os.path.abspath('../../il_5safe'))
+
+project = 'Innovation Lab (5Safe Project)'
+copyright = '2023, Innovation Lab Team'
+author = 'Innovation Lab Team'
+release = '1.0'
+
+autodoc_docstring_signature = True
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
+
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
