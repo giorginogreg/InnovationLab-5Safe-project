554cb4ccdbe5ef0d0484ef2f857b5b197b7f3804
Qubut <s-aahmed@haw-landshut.de>
2023-04-21 22:21:30 +0200

    feat(logging): Setup logger for the project
    created config file for log files location and log colors
    created colored_logger for adding colors to the logger
    created logger for initializing the logger instance

---
+++ il_5safe/utils/logs_utils/logger.py
@@ -0,0 +1,23 @@
+import logging
+from colored_logger import ColoredLogger
+from config import LOG_FILE
+
+logger = ColoredLogger('IL-5Safe', 'main.py')
+
+file_handler = logging.FileHandler(log_file)
+"""
+file_handler: logging.FileHandler
+    The file handler for the logger object.
+"""
+
+file_handler.setLevel(logging.DEBUG)
+
+log_format = logging.Formatter('%(asctime)s - %(message)s', '%Y-%m-%d %H:%M:%S')
+"""
+log_format: logging.Formatter
+    The format for the log messages.
+"""
+
+file_handler.setFormatter(log_format)
+
+logger.addHandler(file_handler)
