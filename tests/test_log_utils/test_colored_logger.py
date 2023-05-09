712fc83babf2e76118d689364445fb5d0d032b41
Qubut <s-aahmed@haw-landshut.de>
2023-05-09 16:06:31 +0200

    test(logger): tested setup_logger for a colored_logger

--- tests/test_log_utils/test_colored_logger.py
+++ tests/test_log_utils/test_colored_logger.py
@@ -1,78 +1,45 @@
-from io import StringIO
 import logging
+import shutil
 import pytest
-from il_5safe.logs_utils.colored_logger import ColoredLogger
+import os
+from typing import Generator
 
-
-from typing import Tuple
+from il_5safe.logs_utils.colored_logger import setup_logger
 
 
 @pytest.fixture
-def colored_logger() -> ColoredLogger:
-    logger = ColoredLogger("il_5safe", "main.py")
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel("DEBUG")
-    logger.addHandler(console_handler)
-    yield logger
+def temp_logs_directory(tmpdir) -> Generator[str, None, None]:
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    # Create a temporary logs directory
+    logs_directory = str(tmpdir.mkdir("logs"))
+    yield logs_directory
+    # Clean up the temporary logs directory
+    shutil.rmtree(logs_directory)
 
 
-@pytest.mark.parametrize(
-    "level, expected_level",
-    [
-        (logging.DEBUG, 10),
-        (logging.INFO, 20),
-        (logging.WARNING, 30),
-        (logging.ERROR, 40),
-        (logging.CRITICAL, 50),
-    ],
-)
-def test_level(
-    colored_logger: ColoredLogger, level: int, expected_level: int
-) -> None:
-    colored_logger.setLevel(level)
-    assert colored_logger.level == expected_level
+def test_setup_logger(temp_logs_directory: str):
+    # Call the setup_logger function with the temporary logs directory
+    logger = setup_logger(log_directory=temp_logs_directory)
 
+    # Test if the logger has been created
+    assert isinstance(logger, logging.Logger)
 
-def test_success(
-    colored_logger: ColoredLogger, caplog: pytest.LogCaptureFixture
-) -> None:
-    caplog.set_level(logging.INFO)
-    colored_logger.success("Task completed successfully.")
-    assert "Task completed successfully." in caplog.text
-    assert caplog.records[0].levelno == logging.INFO
+    # Test if the log file has been created
+    log_files = os.listdir(temp_logs_directory)
+    assert len(log_files) == 1
+    assert log_files[0].startswith("log_") and log_files[0].endswith(".log")
 
+    # Test if the logger has the expected handlers
+    assert len(logger.handlers) == 2
+    assert isinstance(logger.handlers[0], logging.FileHandler)
+    assert isinstance(logger.handlers[1], logging.StreamHandler)
 
-def test_warning(
-    colored_logger: ColoredLogger, caplog: pytest.LogCaptureFixture
-) -> None:
-    caplog.set_level(logging.WARNING)
-    colored_logger.warning("Invalid input received.")
-    assert "Invalid input received." in caplog.text
-    assert caplog.records[0].levelno == logging.WARNING
+    # Test if the log level is set correctly
+    assert logger.level == logging.DEBUG
 
+    # Test if the log message format is set correctly
+    assert isinstance(logger.handlers[0].formatter, logging.Formatter)
+    assert isinstance(logger.handlers[1].formatter, logging.Formatter)
 
-def test_error(
-    colored_logger: ColoredLogger, caplog: pytest.LogCaptureFixture
-) -> None:
-    caplog.set_level(logging.ERROR)
-    colored_logger.error("Failed to connect to database.")
-    assert "Failed to connect to database." in caplog.text
-    assert caplog.records[0].levelno == logging.ERROR
-
-
-def test_critical(
-    colored_logger: ColoredLogger, caplog: pytest.LogCaptureFixture
-) -> None:
-    caplog.set_level(logging.CRITICAL)
-    colored_logger.critical("Unrecoverable error occurred.")
-    assert "Unrecoverable error occurred." in caplog.text
-    assert caplog.records[0].levelno == logging.CRITICAL
-
-
-def test_format_message(colored_logger: ColoredLogger) -> None:
-    colored_logger.level = logging.WARNING
-    message = colored_logger._format_message("Test message")
-    expected_message = (
-        "\033[32mproject_nameWARNING\033[36mmain.pyTest message\033[0m"
-    )
-    assert message == expected_message
+    # Test if the log directory exists
+    assert os.path.exists(temp_logs_directory) and os.path.isdir(temp_logs_directory)
