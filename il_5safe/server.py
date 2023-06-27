a5690402389c9dfb72af7d6c81e24cceec434045
Gregorio Giorgino <giorgino.greg@gmail.com>
2023-06-27 17:07:30 +0200

    Fixed command not working for detecting libs

--- il_5safe/server.py
+++ il_5safe/server.py
@@ -3,25 +3,33 @@ import subprocess
 from flask_cors import CORS
 
 app = Flask(__name__,
-            static_folder='models/yolov5/runs/detect',
-            static_url_path='',
+            static_folder='models/yolov5/runs/detect/',
+            root_path='/app/il_5safe'
+            # static_url_path='static',
             )
 CORS(app)
 
+
 @app.route('/api/predict', methods=['POST'])
 def predict():
-    # Salva l'immagine caricata
     image_file = request.files['image']
-    image_path = 'uploaded_image.jpg'
+    image_path = '/app/upload/uploaded_image.jpg'
     image_file.save(image_path)
 
-    # Esegui lo script di rilevamento degli oggetti
     subprocess.call(
-        ['python', 'models/yolov5/detect.py', '--source', image_path, '--name', 'uploaded_image', '--exist-ok'])
+        [
+            '/opt/poetry-venv/bin/python', 'models/yolov5/detect.py',
+            '--source', image_path,
+            '--name', 'uploaded_image',
+            '--weights', 'il_5safe/resources/weights/yolov5/best_model.pt'
+            '--exist-ok'
+         ],
+        cwd='/app/il_5safe',
+    )
 
-    # Restituisci il risultato
-    result_file = '/uploaded_image/' + image_path
+    result_file = image_path
     return jsonify({'result': result_file})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
