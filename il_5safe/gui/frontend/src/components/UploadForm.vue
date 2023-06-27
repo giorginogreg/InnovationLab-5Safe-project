28e0478e25d52682a3c546998bd2b7b8f8f7df43
Gregorio Giorgino <giorgino.greg@gmail.com>
2023-06-27 17:07:06 +0200

    Fixed url changed

--- il_5safe/gui/frontend/src/components/UploadForm.vue
+++ il_5safe/gui/frontend/src/components/UploadForm.vue
@@ -34,7 +34,7 @@ export default {
         .then(response => {
           // Mostra il risultato
           console.log(response.data.result)
-            this.urlImage = '/static' + response.data.result + '?t=' + Date.now()
+            this.urlImage = response.data.result + '?t=' + Date.now()
         })
         .catch(error => {
           console.error(error)
