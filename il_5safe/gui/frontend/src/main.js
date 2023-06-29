c8bb545d841fa50efa1113137e2e03e7735ba686
Gregorio Giorgino <giorgino.greg@gmail.com>
2023-06-29 11:41:02 +0200

    Added vuetify to the frontend

--- il_5safe/gui/frontend/src/main.js
+++ il_5safe/gui/frontend/src/main.js
@@ -2,12 +2,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router";
 import axios from 'axios'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import VueAxios from 'vue-axios'
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 const app = createApp(App)
-    app.use(router)
+
+app
+    .use(router)
+    .use(vuetify)
     .use(VueAxios, axios)
     .provide('axios', app.config.globalProperties.axios)  // provide 'axios'
     .mount('#app')
